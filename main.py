2c541dab21a56f489b1c435dd7dcc7da2ad4d9e1
zig-gy <benjamin.f.chadwick@proton.me>
2025-04-16 17:56:30 -0400

    locked fps to 60

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
     
     while True:
         for event in pygame.event.get():
@@ -18,7 +21,7 @@ def main():
         
         
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
-
