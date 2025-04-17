e3c3b5d3c9ff6ed4a54cb99c303c3f7aa75b5a4d
zig-gy <benjamin.f.chadwick@proton.me>
2025-04-16 23:49:36 -0400

    added  group logic

--- main.py
+++ main.py
@@ -5,26 +5,31 @@ from player import Player
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         #update
-        player.update(dt)
+        updatable.update(dt)
         
         #render
         screen.fill("black")
-        player.draw(screen)
-
+        for sprite in drawable:
+            sprite.draw(screen)
+        
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
