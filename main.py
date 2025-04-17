d4e106576184be359dd94565bb09a1611b3143f6
zig-gy <benjamin.f.chadwick@proton.me>
2025-04-16 23:33:57 -0400

    added rotation to player controls

--- main.py
+++ main.py
@@ -12,14 +12,17 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+        #update
+        player.update(dt)
+        
+        #render
         screen.fill("black")
-        player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
         player.draw(screen)
 
         pygame.display.flip()
